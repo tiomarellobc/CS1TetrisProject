f6c05d14d44520b9e50d25c65c4b8644db878fbd
Tio Marello <104371398+tiomarellobc@users.noreply.github.com>
2022-05-07 18:04:01 -0400

    added proper l block

--- block.py
+++ block.py
@@ -17,18 +17,14 @@ class block:
         if(letter== "O"):
             self.blockdef= {3:[0,1],4:[0,1]}
         if(letter== "J"):
-<<<<<<< HEAD
-            self.blockdef= {0:[2],1:[2],3:[1,2]}
-=======
             self.blockdef={0:[2],1:[2],3:[1,2]}
             self.color = [0,0,255]
         if(letter == "Z"):
             self.blockdef ={0:[2],1:[1,2],2:[1]}
             self.color = [255,0,0]
         if(letter == "L"):
-            self.blockdef = {0 : [1,2], 1: [2], 2: [2] ,3 : [2]}
+            self.blockdef = {0 : [1,2], 1: [2], 2: [2]}
             self.color = [255, 128, 0]
->>>>>>> caf63a7fa580ba39ce8e2d2a87d6d29b9de84978
     def draw(self):
         for x in self.blockdef:
             for y in self.blockdef[x]:
