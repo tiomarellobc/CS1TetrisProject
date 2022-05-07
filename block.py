c7068529819a862c8c5ebf56080641c7ce4ee204
Tio Marello <104371398+tiomarellobc@users.noreply.github.com>
2022-05-07 16:47:59 -0400

    Added the L

--- block.py
+++ block.py
@@ -7,7 +7,8 @@ class block:
         self.grid = grid
         if(letter == "I"):
             self.blockdef = {2: [0,1,2,3]}
-    
+        if(letter == "L"):
+            self.blockdef = {0 : [1,2], 1 : [1], 2 : [1]}
     def draw(self):
         for x in self.blockdef:
             for y in self.blockdef[x]:
